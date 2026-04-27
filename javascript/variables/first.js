200176ee21bc8d2d18390d4e606d82b82b9d55a7
ashutoshrai7205 <ashutoshrai7205@gmail.com>
2026-04-28 00:12:18 +0530

    Create first.js

---
+++ javascript/variables/first.js
@@ -0,0 +1,29 @@
+let a = 5;
+let b = 10;
+console.log(a + b); // Output: 15
+
+let name = "Ashutosh";
+let age = 20;
+console.log(name, age);
+
+let pi = 3.14;
+console.log(pi); // Output: 3.14
+
+let x = 10;
+let y = 20;
+
+x = x * 2;
+y = y - 10;
+
+let x = 10;
+let y = 20;
+
+[x, y] = [y, x];
+
+console.log(x, y); //swapping
+
+let a = 5;
+let b = 3;
+
+[a, b] = [a + b, a - b];
+// output: a = 8, b = 2
